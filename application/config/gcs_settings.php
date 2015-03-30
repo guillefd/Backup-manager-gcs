8fc017da80e656e25282b2b6f08e480e91b531e9
Guillermo <guillefd@gmail.com>
2015-03-30 17:31:31 -0300

    fix

--- application/config/gcs_settings.php
+++ application/config/gcs_settings.php
@@ -2,8 +2,10 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+# data for Sites Backup
 $config['gcs_app_name'] = 'GCS Backup Manager';
-$config['gcs_client_id'] = '205701658724-heil3mmtfne3ltqkc556q9hn69i6r2tn.apps.googleusercontent.com'; # client id
-$config['gcs_service_account_name'] = '205701658724-heil3mmtfne3ltqkc556q9hn69i6r2tn@developer.gserviceaccount.com'; # email address
-$config['gcs_key_file'] = 'BuscadorEyT-0f667123983b.p12'; # p12 file
+$config['gcs_client_id'] = '469641417329-2vvkqncudf7h8pmg2l6krfrhov2v85bc.apps.googleusercontent.com'; # client id
+$config['gcs_service_account_name'] = '469641417329-2vvkqncudf7h8pmg2l6krfrhov2v85bc@developer.gserviceaccount.com'; # email address
+$config['gcs_key_file'] = 'sites-backup-fa48b8bf1708.p12'; # p12 file
 $config['gcs_oauth_scope'] = 'https://www.googleapis.com/auth/devstorage.full_control';
+
